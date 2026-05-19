41d14c5946d1147965a6159ef5e839c2e466eed4
Lucas Marques <lucaamarques2406@gmail.com>
2026-05-18 22:19:44 -0300

    feat: add Transaction class with value, date and nature

--- src/main/java/com/lucasmarques/sistemabancario/Transaction.java
+++ src/main/java/com/lucasmarques/sistemabancario/Transaction.java
@@ -1,10 +1,28 @@
 package com.lucasmarques.sistemabancario;
 
 import java.math.BigDecimal;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 public class Transaction {
-    BigDecimal value;
-    Date date;
-    int recipient;
+   private final BigDecimal value;
+   private final LocalDateTime date;
+   private final NatureTransaction transictionNature;
+
+    public Transaction(BigDecimal value, NatureTransaction transictionNature) {
+        this.value = value;
+        this.date = LocalDateTime.now();
+        this.transictionNature = transictionNature;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public NatureTransaction getTransictionNature() {
+        return transictionNature;
+    }
 }
